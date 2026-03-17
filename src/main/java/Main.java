ebd89328d000cea9ebd8d3d74fde204ae79349a4
leo <levnsk07@gmail.com>
2026-03-17 11:13:04 +0700

    point for Queue - get func.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import DataBase.Queue;
+import DataBase.User;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
 
         Queue queue = new Queue();
+//        queue.getAll().stream().sorted(Comparator.comparing(User::getPlace)).forEach(System.out::println);
+        queue.getAll().stream().forEach(System.out::println);
+
 
     }
 }
